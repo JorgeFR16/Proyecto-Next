72a72fa76521c0d3e405ad766dd3a58875044368
MarcosA00 <maracastror@gmail.com>
2023-06-05 14:34:09 -0600

    add: Se añaden estilos CSS.

--- pages/components/NavbarLog.tsx
+++ pages/components/NavbarLog.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import '@/proyecto-next/styles/navbarLog.css'
 
 function NavbarLog() {
   return (
     <header>
       <a className='buttonLogo' 
         href=""><img  
-        src={require('../img/atom-logo.png')} 
+        src={require('../img/atom_logo.png')} 
         alt='Logo'
-        width="80px" />
+        width="250px" />
       </a>
       <nav>
-        <ul>
-          <a className='buttonLogIn' href='/#'><li>Iniciar Sesión</li></a>
-          <a className='buttonLogOut' href='/#'><li>Registrarse</li></a>
+        <ul className='logbar'>
+          <a className='buttonLogin' 
+            href=''><li>Iniciar Sesión</li>
+          </a>
+          <a className='buttonLogout' 
+            href=''><li>Registrarse</li>
+          </a>
         </ul>
       </nav>
     </header>
