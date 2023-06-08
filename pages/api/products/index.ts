17c2f7318b461c35899b6a77de9866706fa8dcef
jorge.flores <jorgefr_1693@hotmail.com>
2023-06-08 14:36:11 -0600

    add: Se agregan los archivos y configuraciones necesarios para poder manejar la informacion de juegos de la BD por medio de /api/productos

---
+++ pages/api/products/index.ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { createProduct, deleteProduct, getAllProducts } from '@/prisma/product'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  
+    try {
+        
+        switch (req.method) {
+            case 'POST': {
+
+                const {desarrollador, disponibilidad, distribuidor, genero, imagen, nombre_juego, precio} = req.body;
+
+                const new_product = await createProduct({nombre_juego, genero, precio, desarrollador, distribuidor, disponibilidad,imagen});
+
+                return res.status(201).json(new_product);
+
+            }
+
+            case 'GET': {
+
+                const products = await getAllProducts()
+
+                return res.status(200).json(products);
+
+            }
+
+            case 'DELETE': {
+
+                const { id } = req.query
+
+                // el id lo va a buscar a esta direccion /Api/products?id=54654
+
+                await deleteProduct(id)
+
+                return res.status(200).json({message: 'Juegos borrado exitosamente'});
+
+            }
+                
+        }
+
+    } catch (error) {
+        
+    }
+    
+}
